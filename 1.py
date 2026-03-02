78fd90135680e5fbf9928b18cee78cd74b93466e
RANINDRAM123 <ranindramduttadev.123@gmail.com>
2026-03-03 03:13:29 +0530

    message

--- 1.py
+++ 1.py
@@ -8,10 +8,12 @@ df["timestamp"] = pd.to_datetime(df["timestamp"])
 for shed in df["location"].unique():
     d = df[df["location"] == shed]
     gap = d["timestamp"].diff().dt.total_seconds() / 60
+    missed = gap[gap <= 5]
     danger = gap[gap > 10]
     print(danger)
-    completness = 100 - (len(danger)/len(d))*100
+    completness = 100 - ((len(danger)+len(missed))/len(d))*100
     print("Shed:", shed)
     print("Ttal reading: ", len(d))
-    print("Missing: ",len(danger))
+    print("Danger : ",len(danger))
     print("%", completness)
+    print("Missed : ", len(missed))
